fc47f88ed2a7dcfe1d718d84c63eb531dacd2528
Win Chan <winnie.chanszewing@gmail.com>
2026-02-05 02:22:28 +0100

    update buttons, banners, and fix changing theme across page

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+
+
 function toggleTheme() {
     document.body.classList.toggle('light-mode');
+    const isLight = document.body.classList.contains('light-mode'); // Define isLight HERE
+
+    sessionStorage.setItem('theme', isLight ? 'light' : 'dark');
+    console.log('Theme saved:', sessionStorage.getItem('theme'));
 }
+
+window.addEventListener('DOMContentLoaded', function () {
+    const savedTheme = sessionStorage.getItem('theme');
+    console.log('Saved theme on load:', savedTheme);
+    const isLight = savedTheme === 'light';
+
+    if (isLight) {
+        document.body.classList.add('light-mode');
+        console.log('Applied light mode');
+    }
+});
