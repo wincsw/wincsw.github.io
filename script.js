dcc827787349b02843d00f3d162fe6860e036409
wincsw <winnie.chanszewing@gmail.com>
2026-08-25 19:54:23 +0200

    finish mushroom page

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 
+var r = document.querySelector(':root');
+
+function lightTheme() {
+    r.style.setProperty('--ink', 'black');
+    r.style.setProperty('--bgcolor', 'white');
+    document.getElementById("me_image").src = "images/me_body.png";
+    console.log('set light mode');
 
 
-function toggleTheme() {
-    document.body.classList.toggle('light-mode');
-    const isLight = document.body.classList.contains('light-mode'); // Define isLight HERE
-
-    sessionStorage.setItem('theme', isLight ? 'light' : 'dark');
-    console.log('Theme saved:', sessionStorage.getItem('theme'));
 }
 
-window.addEventListener('DOMContentLoaded', function () {
-    const savedTheme = sessionStorage.getItem('theme');
-    console.log('Saved theme on load:', savedTheme);
-    const isLight = savedTheme === 'light';
+function darkTheme() {
+    r.style.setProperty('--ink', 'white');
+    r.style.setProperty('--bgcolor', 'black');
+    document.getElementById("me_image").src = "images/me_body_black.png";
+    console.log('set dark mode');
 
-    if (isLight) {
-        document.body.classList.add('light-mode');
-        console.log('Applied light mode');
+}
+
+function toggleTheme() {
+    if (sessionStorage.getItem('theme') == 'light') {
+        darkTheme();
+        sessionStorage.setItem('theme', 'dark');
     }
-});
+    else {
+        lightTheme();
+        sessionStorage.setItem('theme', 'light');
+
+    }
+
+    console.log('Theme saved change');
+    
+
+}
+
+// window.addEventListener('DOMContentLoaded', function () {
+//     const savedTheme = sessionStorage.getItem('theme');
+//     console.log('Saved theme on load:', savedTheme);
+//     const isLight = savedTheme === 'light';
+
+//     if (sessionStorage.getItem('theme') == "light") {
+//         lightTheme()
+//         console.log('Applied light mode');
+//     }
+//     else {
+//         darkTheme();
+//         console.log('Applied dark mode');
+
+//     }
+// });
